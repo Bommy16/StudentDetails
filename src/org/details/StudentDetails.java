1f5d5c42777d68c489467b5bcb70a3ce025b7141
Bommy16 <sujitha16.vs@gmail.com>
2021-10-17 19:32:37 +0530

    Student Details push up

---
+++ src/org/details/StudentDetails.java
@@ -0,0 +1,29 @@
+package org.details;
+
+public class StudentDetails {
+	public static void main(String[] args) {
+		//data type variableName=value;
+		byte age=23;
+		short pin=1607;
+		int id=1234567890;
+		float f=7.000007f;
+		long mobNo=9876543210l;
+		double d=5.00071607000007;
+		char c='s';
+		boolean b=true;
+		String email="abcdefgh.07@gmail.com";
+		String password="**********";
+		
+		
+		System.out.println("Age is : " + age);
+		System.out.println("pin is : " + pin);
+		System.out.println("Id No : " + id);
+		System.out.println(f);
+		System.out.println("Mob No : " + mobNo);
+		System.out.println(d);
+		System.out.println(c);
+		System.out.println(b);
+		System.out.println("Email=" + email);
+		System.out.println("Password: " + password);
+	}
+}
